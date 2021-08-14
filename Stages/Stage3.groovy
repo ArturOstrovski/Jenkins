c23563e5224d0332510c60e4d0abbe5c14043abf
Jaxseras <74349815+Jaxseras@users.noreply.github.com>
2021-08-14 14:17:13 +0300

    Update Stage3.groovy

--- Stages/Stage3.groovy
+++ Stages/Stage3.groovy
@@ -1,6 +1,13 @@
 def PrintFreeSpace() {
 
-    println(fsutil volume diskfree C:)
+    File diskPartition = new File("C:");
+    long totalCapacity = diskPartition.getTotalSpace(); 
+    long freePartitionSpace = diskPartition.getFreeSpace(); 
+    long usablePatitionSpace = diskPartition.getUsableSpace(); 
+    
+    System.out.println("Total C partition size : " + totalCapacity / (1024*1024*1024) + " GB");
+    System.out.println("Usable Space : " + usablePatitionSpace / (1024 *1024*1024) + " GB");
+    System.out.println("Free Space : " + freePartitionSpace / (1024 *1024*1024) + " GB");
     
 }
 return this
