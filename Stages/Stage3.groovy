78ffc8471a2be20121827dbec67fdc85db3f2152
Jaxseras <74349815+Jaxseras@users.noreply.github.com>
2021-08-14 17:05:29 +0300

    Update Stage3.groovy

--- Stages/Stage3.groovy
+++ Stages/Stage3.groovy
@@ -1,6 +1,6 @@
 def free_Space () {
     
-    def free_disk_size = bat(script: "fsutil volume diskfree C:/Jenkins/workspace").trim()
+    def free_disk_size = bat(script: "fsutil volume diskfree C:/Jenkins/workspace")
     println free_disk_size
 
     }
