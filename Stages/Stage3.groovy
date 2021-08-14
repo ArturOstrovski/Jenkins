bbb73000fe68daaad7ec5026c20a5cccb1333018
Jaxseras <74349815+Jaxseras@users.noreply.github.com>
2021-08-14 15:36:55 +0300

    Update Stage3.groovy

--- Stages/Stage3.groovy
+++ Stages/Stage3.groovy
@@ -6,13 +6,15 @@ import java.io.File
     def free_Spacesss () {
  
         def diskPartition = new File("C:")
- 
+        
+        def sampleDirSize = diskPartition.directorySize()
         long totalCapacity = diskPartition.getTotalSpace() 
  
         long freePartitionSpace = diskPartition.getFreeSpace()
         long usablePatitionSpace = diskPartition.getUsableSpace()
- 
-        println("Total C partition size : " + $totalCapacity / (1024*1024*1024) + " GB")
+        
+        println("Total C partition size : " + sampleDirSize + " GB")
+        println("Total C partition size : " + totalCapacity / (1024*1024*1024) + " GB")
         println("Usable Space : " + usablePatitionSpace / (1024 *1024*1024) + " GB")
         println("Free Space : " + freePartitionSpace / (1024 *1024*1024) + " GB")
     }
